0a2c41a16b9f835c50d81234ce8b9a2c24ec0089
Elias <eliaswu466@gmail.com>
2025-09-07 19:32:12 -0400

    added arguments

--- src/discord/webhook_setup.py
+++ src/discord/webhook_setup.py
@@ -3,11 +3,11 @@ from config import DISCORD_WEBHOOK_URL
 
 def send_webhook_message(data: dict):
     content = (
-        f"Title: {data['title']}\n"
-        f"Price: {data['price']}\n"
-        f"Mileage: {data['mileage']}\n"
-        f"Transmission: {data['transmission']}"
-    )
+    f"Title: {data.get('title', 'Unknown Title')}\n"
+    f"Price: {data.get('price', 'N/A')}\n"
+    f"Mileage: {data.get('mileage', 'N/A')}\n"
+    f"Transmission: {data.get('transmission', 'N/A')}"
+)
     response = requests.post(
         DISCORD_WEBHOOK_URL,
         json={"content": content}
--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
+import argparse
+
 from driver_setup import create_driver
 from login import login
 from marketplace_scraper import get_listing_urls, scrape_listing
 from discord.webhook_setup import send_webhook_message
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--limit', type = int, help = "Enter how many listings you would like", default = 10)
+    parser.add_argument('--headless', type = bool, help = "Enter True or False for headless", default = False)
 
-    driver = create_driver()
+    args = parser.parse_args()
+
+    limit = args.limit
+    headless = args.headless
+
+    driver = create_driver(headless = headless)
     try:
         login(driver)
-        urls = get_listing_urls(driver, max_links=10)
+        urls = get_listing_urls(driver, max_links = limit)
 
         # scrape information (title, price, location, etc.) from each listing
         # and send it to the Discord webhook
